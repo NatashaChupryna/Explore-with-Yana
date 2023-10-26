627b0218be2fb4f0ad40fbcfb80b10a3a74ebb18
Natasha <natkamonte1992@gmail.com>
2023-10-27 00:07:09 +0300

    edits

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// Banner script
 function banner() {
   const overlay = document.getElementById("overlay");
   overlay.style.display = "block";
 }
-
 setTimeout(banner, 15000);
+
+// Form script
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  modal: document.querySelector("[data-modal]"),
+  body: document.querySelector("[data-page]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+  refs.body.classList.toggle("no-scroll");
+}
