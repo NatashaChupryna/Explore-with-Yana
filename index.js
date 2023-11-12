f835137f17775c147ccb8b46d34ec4c83be13947
Natasha <natkamonte1992@gmail.com>
2023-11-12 11:57:53 +0200

    add form and header

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+"use strict";
 // Banner script
+// function banner() {
+//   const overlay = document.getElementById("overlay");
+//   overlay.style.display = "block";
+// }
+// setTimeout(banner, 15000);
+
 function banner() {
   const overlay = document.getElementById("overlay");
-  overlay.style.display = "block";
+  const hasVisited = localStorage.getItem("hasVisited");
+
+  if (!hasVisited) {
+    overlay.style.display = "block";
+    localStorage.setItem("hasVisited", true);
+  } else {
+    overlay.style.display = "none";
+  }
 }
+
 setTimeout(banner, 15000);
 
 // Form script
@@ -20,3 +35,86 @@ function toggleModal() {
   refs.modal.classList.toggle("is-hidden");
   refs.body.classList.toggle("no-scroll");
 }
+
+// Mobile menu
+// (() => {
+//   const menuOpenBtn = document.querySelector("[data-menu-open]");
+//   const menuCloseBtn = document.querySelector("[data-menu-close]");
+
+//   const mobileMenu = document.querySelector("[data-menu]");
+//   // const body = document.querySelector('body');
+
+//   menuOpenBtn.addEventListener("click", openModal);
+//   menuCloseBtn.addEventListener("click", openModal);
+
+//   function openModal() {
+//     mobileMenu.classList.toggle("is-open");
+//     body.classList.toggle("no-scroll");
+//   }
+// })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementsByClassName("modal-form")[0];
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+
+    if (error === 0) {
+      form.classList.add("_sending");
+      if (form.classList.contains("sending")) {
+        alert("Thanks. I`ll get in contact with you");
+      }
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        let result = await response.json();
+        form.reset();
+      }
+    } else {
+      alert("Fill in required fields");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = form.querySelectorAll("._req");
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      }
+
+      if (input.value === "") {
+        formAddError(input);
+        error++;
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+});
---
+++ sendMail.php
@@ -0,0 +1,46 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "./phpMailer/src/Exception";
+require "./phpMailer/src/PHPMailer";
+
+$mail = new PHPMailer(true);
+$mail -> Charset = 'UTF-8';
+$mail -> isHTML = true;
+
+$mail -> addAddress('yanatraveladvisor@gmail.com');
+$mail -> Subject = 'Email from Plan Voyage';
+
+
+$body = '<h1>New client</h1>';
+
+if(trim(!empty($_POST['name'].))){
+    $body.='<p><strong> Name : '.$_POST['name'].'</strong></p>';
+}
+if(trim(!empty($_POST['email'].))){
+    $body.='<p><strong> Email : '.$_POST['email'].'</strong></p>'
+};
+if(trim(!empty($_POST['phone'].))){
+    $body.='<p><strong> phone : '.$_POST['phone'].'</strong></p>'
+};
+if(trim(!empty($_POST['numder of travellers'].))){
+    $body.='<p><strong> numder of travellers : '.$_POST['numder of travellers'].'</strong></p>'
+};
+if(trim(!empty($_POST['destination'].))){
+    $body.='<p><strong> destination : '.$_POST['destination'].'</strong></p>'
+};
+
+$mail -> body = $body;
+
+if(!$mail -> send()){
+    $message = 'Error';
+} else {
+    $message = 'Information was sent';
+}
+
+$response = ['message'=> $message];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
